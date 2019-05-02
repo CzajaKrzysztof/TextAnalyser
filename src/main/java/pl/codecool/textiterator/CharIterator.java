e47cc0ea2e99f9afd0242366ba7f9931f935935d
Krzysiek <krzysiekczaja@gmail.com>
2019-05-02 20:44:08 +0200

    Add CharIterator with all its classes

--- src/main/java/pl/codecool/textiterator/CharIterator.java
+++ src/main/java/pl/codecool/textiterator/CharIterator.java
@@ -4,9 +4,11 @@ import java.util.Iterator;
 
 public class CharIterator implements Iterator<String> {
     String[] fileContentList;
+    int index;
 
     public CharIterator(String fileContent) {
         fileContentList = splitContentToList(fileContent);
+        index = 0;
     }
 
     private String[] splitContentToList(String fileContent) {
@@ -15,11 +17,11 @@ public class CharIterator implements Iterator<String> {
 
     @Override
     public boolean hasNext() {
-        return false;
+        return index < fileContentList.length;
     }
 
     @Override
     public String next() {
-        return null;
+        return fileContentList[index++];
     }
 }
