4da9af07c62f1b25d1c8002018b9686cbb3f799f
Krzysiek <krzysiekczaja@gmail.com>
2019-05-03 12:11:06 +0200

    Add methods to print out string, number, map and collection

--- src/main/java/pl/codecool/view/View.java
+++ src/main/java/pl/codecool/view/View.java
@@ -1,4 +1,17 @@
 package pl.codecool.view;
 
+import java.util.Collection;
+
 public class View {
+    public void print(String string) {
+        System.out.println(string);
+    }
+
+    public void print(int number) {
+        System.out.println(number);
+    }
+
+    public void print(Collection<String> collection) {
+        System.out.println(collection.toString());
+    }
 }
