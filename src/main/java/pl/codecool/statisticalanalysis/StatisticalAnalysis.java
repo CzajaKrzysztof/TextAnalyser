8f6f8fdd3581a49a1b72fdefab5fcca31eace1e5
Krzysiek <krzysiekczaja@gmail.com>
2019-05-03 10:47:05 +0200

    Add method to create dictionary from file content

--- src/main/java/pl/codecool/statisticalanalysis/StatisticalAnalysis.java
+++ src/main/java/pl/codecool/statisticalanalysis/StatisticalAnalysis.java
@@ -1,4 +1,44 @@
 package pl.codecool.statisticalanalysis;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class StatisticalAnalysis {
+    Iterator<String> iterator;
+    TreeMap<String, Integer> dictionary;
+
+    public StatisticalAnalysis(Iterator<String> iterator) {
+        this.iterator = iterator;
+        dictionary = createDictionary();
+    }
+
+    private TreeMap<String, Integer> createDictionary() {
+        TreeMap<String, Integer> tempDict = new TreeMap<>();
+
+        while(iterator.hasNext()) {
+            String key = iterator.next();
+            tempDict.put(key, tempDict.getOrDefault(key, 0) + 1);
+        }
+
+        return tempDict;
+    }
+
+    public int countOf(String... strings) {
+        int count =0;
+
+        return count;
+    }
+
+    public int dictionarySize() {
+        return dictionary.size();
+    }
+
+    public int size() {
+
+    }
+
+    public Set<String> occurMoreThen(int) {
+
+    }
 }
